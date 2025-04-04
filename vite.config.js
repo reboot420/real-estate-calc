9ec5e17edc32c003a832a1ea463aada300b30223
reboot420 <freedom88san@outlook.jp>
2025-04-05 06:36:15 +0900

    Vite設定ファイルにビルド設定を追加

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/real-estate-calc/'
+  base: '/real-estate-calc/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
